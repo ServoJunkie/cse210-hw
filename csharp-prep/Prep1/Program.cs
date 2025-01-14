9aa5f4ed8b8f0f15382ab9ce5be1db0106c5cd5f
ServoJunkie <joh20048@byui.edu>
2025-01-13 19:28:22 -0700

    Changed Prep1 to ask for the user's first and last name, and then print them out for the user to see.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //The next four lines write out requests to ask the user for their first and last names, and to store them in strings for later use.
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        //These next 2 lines write a blank line in the terminal, as well as a line that includes the strings obtained by the user.
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
