b0290d6fde44fc745d6c6d849a5f3813bc6f7c01
ServoJunkie <joh20048@byui.edu>
2025-01-13 20:40:19 -0700

    Changed Prep2 to ask for the user's grade percentage, write their letter grade to the terminal, and write to the terminal to inform the user if they have passed the class.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //This block of code asks the user for their grade percentage, stores their response, and converts it to a float.
+        Console.Write("Please enter you grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        float numberPercentage = int.Parse(gradePercentage);
+        Console.WriteLine();
+
+        //This code defines 'letter' as a string variable, sets it equal to the proper letter depending on the user's percentage, and 
+        //writes it to the terminal.
+        string letter;
+        if (numberPercentage >= 90)
+            {
+                letter = "A";
+            }
+        else if (numberPercentage >= 80)
+            {
+                letter = "B";
+            }
+        else if (numberPercentage >= 70)
+            {
+                letter = "C";
+            }
+        else if (numberPercentage >= 60)
+            {
+                letter = "D";
+            }
+        else
+            {
+                letter = "F";
+            }
+        Console.WriteLine($"Your letter grade is {letter}");
+        
+        //This if-else statement writes a statement declaring whether the user has passed the course.
+        if (numberPercentage >= 70)
+            {
+                Console.WriteLine("You have passed the course!");
+            }
+        else
+            {
+                Console.WriteLine("You did not pass the course. Please try again! I know you can do better next time!");
+            }
     }
 }
