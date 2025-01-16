61689b072792d8f4c68c28d4c4fabd454aecf6a2
ServoJunkie <joh20048@byui.edu>
2025-01-16 13:37:38 -0700

    Wrote a code in Prep3 to generate a random number and ask the user for guesses until they guess correctly.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //First, we generate a random number for the user to guess.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //We set a boolean variable in order to detect if the user has guessed correctly.
+        bool guessedCorrectly = false;
+
+        //This code asks for the user to input a guess and compares it to the magic number. It will loop until the user guesses correctly.
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guessedCorrectly = true;
+            }
+        } while (guessedCorrectly == false);
     }
 }
