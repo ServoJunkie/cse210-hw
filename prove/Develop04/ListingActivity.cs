4ed0fb535abdb53c6311e7fef8aac83803e01c4c
ServoJunkie <joh20048@byui.edu>
2025-04-01 13:49:46 -0600

    Deleted duplicate variable

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,7 +1,6 @@
 public class ListingActivity : Activity
 {
     private List<string> _prompts = new List<string>{"Who are people that you appreciate?", "What are personal strengths of yours?", "Who are people that you have helped this week?", "When have you felt the Holy Ghost this month?", "Who are some of your personal heroes?"};
-    private int numberOfItemsEntered;
 
     public ListingActivity()
     {
